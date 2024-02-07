c7308a06cdbfc17464d95deda18fa3d9e86e55e2
harrisbang <harrisbang2@gmail.com>
2024-02-07 21:30:24 +0900

    Comment Delete/insert/update responseBody 추가

--- src/main/java/com/sparta/duopleaseduo/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/duopleaseduo/dto/CommentResponseDto.java
@@ -2,10 +2,12 @@ package com.sparta.duopleaseduo.dto;
 
 import com.sparta.duopleaseduo.entity.Comment;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class CommentResponseDto {
 
     private Long id;
--- src/test/java/com/sparta/duopleaseduo/CommentTest.java
+++ src/test/java/com/sparta/duopleaseduo/CommentTest.java
@@ -14,6 +14,8 @@ import org.springframework.test.annotation.Rollback;
 public class CommentTest {
     @Autowired
     private CommentService service;
+
+    /*                       성공 코드들                       */
     @Test
     @DisplayName("1차 캐시 : comment 저장")
     @Rollback(value = false)
@@ -45,6 +47,45 @@ public class CommentTest {
     @Rollback(value = false)
     void deleteComment(){
         CommentResponseDto res = service.deleteComment(2L);
+        System.out.println("==========================================================================================");
+        System.out.println(res.getCode());
+        System.out.println(res.getStatus());
+    }
+    
+    
+    ///
+    /*                       싶패 코드들                       */
+//    @Test
+//    @DisplayName("1차 캐시 : comment 저장")
+//    @Rollback(value = false)
+//    void createCommentfail(){
+//        CommentRequestDto requestDto = new CommentRequestDto();
+//        requestDto.setComment("1차 캐시 : comment 저장");
+//
+//        CommentResponseDto res = service.createComment(requestDto);
+//        System.out.println("==========================================================================================");
+//        System.out.println(res.getCode());
+//        System.out.println(res.getStatus());
+//    }
+    @Test
+    @DisplayName("2차 캐시 : comment 변경")
+    @Rollback(value = false)
+    void updateCommentfail(){
+        CommentRequestDto requestDto = new CommentRequestDto ();
+        requestDto.setComment("2차 캐시 : comment 변경");
+        //LocalDateTime localDate = LocalDateTime.now();
+        //requestDto.setCreatedAt(localDate);
+        CommentResponseDto res = service.updateComment(3100L,requestDto);
+        System.out.println("==========================================================================================");
+        System.out.println(res.getCode());
+        System.out.println(res.getStatus());
+    }
+    //
+    @Test
+    @DisplayName("3차 캐시 : comment 삭제")
+    @Rollback(value = false)
+    void deleteCommentfail(){
+        CommentResponseDto res = service.deleteComment(900L);
         System.out.println("==========================================================================================");
         System.out.println(res.getCode());
         System.out.println(res.getStatus());
