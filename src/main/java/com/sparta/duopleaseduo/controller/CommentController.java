18d1d2c346d641dc0e6e826b0c775696524bd421
harrisbang <harrisbang2@gmail.com>
2024-02-08 16:10:58 +0900

    Comment ResponseDto 변경 + ResponseBody 식으로 변경 + Test 에 User 랑 Feed 넣고 테스트 확인.

--- src/main/java/com/sparta/duopleaseduo/controller/CommentController.java
+++ src/main/java/com/sparta/duopleaseduo/controller/CommentController.java
@@ -1,21 +1,35 @@
 package com.sparta.duopleaseduo.controller;
 
 import com.sparta.duopleaseduo.dto.CommentRequestDto;
-import com.sparta.duopleaseduo.dto.CommentResponseDto;
 import com.sparta.duopleaseduo.service.CommentService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hibernate.query.sqm.tree.SqmNode.log;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
-    @Autowired
+
     private CommentService service;
     /// add comments
     @PostMapping("/comment/create")
-    public CommentResponseDto CreateComment(@RequestBody CommentRequestDto requestDto) {
-        return service.createComment(requestDto);
+    public ResponseEntity<?> CreateComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            log.info("댓글 작성 오류");
+            return ResponseEntity.badRequest()
+                    .body(createErrorMessages(bindingResult));
+        }
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.createComment(requestDto,request));
     }
 
     /// get comments
@@ -25,12 +39,31 @@ public class CommentController {
 //    }
     ///
     @PatchMapping("/comment/{commentId}")
-    public CommentResponseDto updateComment(@PathVariable(name = "commentId") Long id, CommentRequestDto requestDt) {
-        return service.updateComment(id,requestDt);
+    public ResponseEntity<?> updateComment(@PathVariable(name = "commentId") Long id, CommentRequestDto requestDt,HttpServletRequest request,BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            log.info("댓글 수정 오류");
+            return ResponseEntity.badRequest()
+                    .body(createErrorMessages(bindingResult));
+        }
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.updateComment(id,requestDt,request));
     }
     // deleting the item.
     @DeleteMapping("/comment/{commentId}")
-    public CommentResponseDto deleteComment(@PathVariable(name = "commentId") Long id) {
-        return service.deleteComment(id);
+    public ResponseEntity<?> deleteComment(@PathVariable(name = "commentId") Long id,HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.deleteComment(id,request));
     }
+
+
+
+    // error message
+    private List<String> createErrorMessages(BindingResult bindingResult) {
+        return bindingResult.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+    }
+
+
 }
