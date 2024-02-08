fc6f95e9aff76bef84337a0c3ad1d690ef99c9fe
yallo428 <yallo428@naver.com>
2024-02-08 16:57:32 +0900

    refactor : commentRepository

--- src/main/java/com/sparta/duopleaseduo/repository/CommentRepository.java
+++ src/main/java/com/sparta/duopleaseduo/repository/CommentRepository.java
@@ -1,11 +1,14 @@
 package com.sparta.duopleaseduo.repository;
 
 import com.sparta.duopleaseduo.entity.Comment;
+import com.sparta.duopleaseduo.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-
+    List<Comment> findAllByFeed(Feed feed);
 }
