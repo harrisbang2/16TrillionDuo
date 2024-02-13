ddf7f4dc9e01f7b6c6f840acef8fd0910b4df64c
yallo428 <yallo428@naver.com>
2024-02-13 19:37:43 +0900

    Update FeedFormDto.java

--- src/main/java/com/sparta/duopleaseduo/dto/request/FeedFormDto.java
+++ src/main/java/com/sparta/duopleaseduo/dto/request/FeedFormDto.java
@@ -1,5 +1,6 @@
 package com.sparta.duopleaseduo.dto.request;
 
+import com.sparta.duopleaseduo.entity.Feed;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -9,4 +10,10 @@ public class FeedFormDto {
     private String title;
     @NotBlank(message = "내용을 적어주세요.")
     private String contents;
+
+
+    public FeedFormDto(Feed feed) {
+        this.title = feed.getTitle();
+        this.contents = feed.getContent();
+    }
 }
